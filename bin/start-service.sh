c426aa4edba636b390d6d2f80309b7e045cb052e
Yan Marques <marques_yan@outlook.com>
2019-11-10 11:24:09 -0300

    Fix start-service script to handle active configuration

--- bin/start-service.sh
+++ bin/start-service.sh
@@ -14,13 +14,13 @@ shift
 
 if [ ! -f "$ENV_FILE" ]; then
     cp .env.example "$ENV_FILE"
+
+    export FLASK_APP="$APP_NAME:create_app()"
+    flask gen:key "$ENV_FILE"
 fi 
 
 dotenv -f "$ENV_FILE" -q never set FLASK_DEBUG 0 
 dotenv -f "$ENV_FILE" set FLASK_ENV production
 dotenv -f "$ENV_FILE" set FLASK_RUN_HOST 0.0.0.0
 
-export FLASK_APP="$APP_NAME:create_app()"
-
-flask gen:key "$ENV_FILE"
-APP_WSGI="$APP_NAME" gunicorn wsgi:app
+APP_WSGI="$APP_NAME" gunicorn wsgi:app "$@"
