285ddeea08dfdd5d0ad7b57048d1c0eaa4446290
Yan Marques de Cerqueira <marques_yan@outlook.com>
2020-08-16 16:14:56 -0300

    Fix blocks not using proof as attribute anymore

--- tests/util.py
+++ tests/util.py
@@ -5,7 +5,6 @@ from vpngate.p2p import Peer
 
 def assert_is_genesis_block(block):
     assert block.previous_hash == '1'
-    assert block.proof == 100
     assert block.index == 0
     assert block.timestamp == -1
 
@@ -13,7 +12,6 @@ def assert_is_genesis_block(block):
 def get_block(**kwargs) -> Block:
     kwargs.setdefault('index', 1)
     kwargs.setdefault('transactions', [])
-    kwargs.setdefault('proof', 123)
     kwargs.setdefault('previous_hash', '321')
     kwargs.setdefault('timestamp', None)
     return Block(**kwargs)
