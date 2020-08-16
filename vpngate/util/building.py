9087fb3fca64be363c37bbb78ea818352613ef5f
Yan Marques de Cerqueira <marques_yan@outlook.com>
2020-08-16 01:35:26 -0300

    Add proof of work block

--- vpngate/util/building.py
+++ vpngate/util/building.py
@@ -8,28 +8,37 @@ class Block:
     This class represents each component in the chain. It holds data
     about:
         - last block in chain 
-        - the proof to create the block
         - the actual content in the transactions
         - when the block was assigned 
     """
 
     index: int
     transactions: list
-    proof: int
     previous_hash: str
     timestamp: time.time = field(default_factory=time.time)
 
     @classmethod
-    def genesis(cls):
+    def genesis(cls, **kwargs):
         """Create the default first block in the chain"""
 
-        return cls(index=0,
-                   transactions=[],
-                   proof=100,
-                   previous_hash='1',
-                   timestamp=-1)
+        defaults = dict(index=0,
+                        transactions=[],
+                        previous_hash='1',
+                        timestamp=-1)
+
+        return cls(defaults.update(**kwargs))
                 
     def to_dict(self) -> dict:
         """Return a dict representing this dataclass."""
 
         return asdict(self)
+
+
+@dataclass
+class PoWBlock(Block):
+    proof: int
+
+    @classmethod
+    def genesis(cls, **kwargs):
+        kwargs.setdefault('proof', 100)
+        return super().genesis(**kwargs)
