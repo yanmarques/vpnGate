194c55d8b697b1168a8b0e80821b38cbcdfb1858
Yan Marques de Cerqueira <marques_yan@outlook.com>
2020-08-16 00:39:23 -0300

    Refactor p2p Peer implementation

--- vpngate/p2p.py
+++ vpngate/p2p.py
@@ -1,10 +1,19 @@
 from .util import crypto
+
 from typing import Set, Any
 from dataclasses import dataclass, field
 
 
 @dataclass
 class Peer:
-    identifier: field(default_factory=crypto.hex_uuid)
-    children: Set[Any]
-    parent: Any
+    address: str
+    identifier: str = field(default_factory=crypto.hex_uuid)
+    children: Set[Any] = field(default_factory=set)
+    siblings: Set[Any] = field(default_factory=set)
+    parent: Any = None
+
+    def __eq__(self, another_peer):
+        return self.identifier == another_peer.identifier
+
+    def __hash__(self):
+        return int(self.identifier, 16)
